8ba25fb58ccad57e15f118f61a4e4e42e4c2dba0
easton-bittner <46513006+easton-bittner@users.noreply.github.com>
2019-01-14 18:59:02 -0700

    Update webpageDownload.py
    begin the process, does not compile

--- webpages/webpageDownload.py
+++ webpages/webpageDownload.py
@@ -7,34 +7,34 @@ import asyncio
 import async_timeout
 
 async def fetch(session, url):
-    with async_timeout.timeout(10):
+    with async_timeout.timeout(30):
         async with session.get(url) as response:
             return await response.text()
 
 async def get_character_select(loop):
     async with aiohttp.ClientSession(loop=loop) as session:
-        character_select_url = 'http://rbnorway.org/t7-frame-data/'
+        character_select_url = 'http://geppopotamus.info/game/tekken7fr/'
         response = await fetch(session, character_select_url)
         soup = BeautifulSoup(response, "html.parser")
 
     for div in soup.select('div.entry.clearfix'):
         all_urls = div.findAll('a')
         for link in all_urls:
-            if 't7-frames'not in link.get('href'):
+            if 'tekken7fr'not in link.get('href'):
                 continue
             else:
                 print('Found something.')
                 webpage_downloader(link.get('href'))
 
 def webpage_downloader(url):
-    base_url = 'http://rbnorway.org/'
+    base_url = 'http://geppopotamus.info/'
     character_url = url
 
     if base_url not in character_url:
         character_url = urljoin(base_url,character_url)
 
-    file_name = character_url.replace('http://rbnorway.org/', '')
-    file_name = file_name.replace('-t7-frames', '')
+    file_name = character_url.replace('http://geppopotamus.info/', '')
+    file_name = file_name.replace('tekken7fr', '')
     file_name = file_name.replace('/', '')
     file_name = file_name.replace('-', '_')
     file_name = file_name.lower()
@@ -55,5 +55,4 @@ loop.run_until_complete(get_character_select(loop))
 #                 continue
 #             else:
 #                 webpage_downloader(link.get('href'))
-
 
